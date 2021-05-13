2aa999775705cc64d5037a60c1017057e09d4542
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2021-05-13 16:29:45 -0400

    fix missing export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as newValidator } from './validator'
 export { default as validateNotBlank } from './validateNotBlank'
 export { default as validateRequired  } from './validateRequired'
+export { default as validateUrl } from './validateUrl'
