b56c0f09443d5494f01de9af874f41bdd957502c
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2021-05-12 23:34:25 -0400

    add validateUrl

---
+++ tests/validateUrl.spec.js
@@ -0,0 +1,59 @@
+import newValidator from '@/validator'
+import validateUrl from '@/validateUrl'
+
+describe('validateUrl', () => {
+  describe('with valid url', () => {
+
+    it('sets isValid to true', () => {
+      let validator = newValidator({url: 'https://github.com/gregors' })
+      validator = validateUrl(validator, 'url')
+
+      expect(validator.isValid).toBe(true)
+    })
+
+    it('with query params sets isValid to true', () => {
+      let validator = newValidator({url: 'https://github.com/gregors?boom=true' })
+      validator = validateUrl(validator, 'url')
+
+      expect(validator.isValid).toBe(true)
+    })
+
+    it('with ip sets isValid to true', () => {
+      let validator = newValidator({url: 'https://127.0.0.1' })
+      validator = validateUrl(validator, 'url')
+
+      expect(validator.isValid).toBe(true)
+    })
+  })
+
+  describe('with invalid url', () => {
+
+    it('sets isValid to false', () => {
+      let validator = newValidator({url: 'https/github.com/gregors' })
+      validator = validateUrl(validator, 'url')
+
+      expect(validator.isValid).toBe(false)
+    })
+
+    it('adds message', () => {
+      let validator = newValidator({url: 'https/github.com/gregors' })
+      validator = validateUrl(validator, 'url')
+
+      expect(validator.messages.length).toBe(1)
+      expect(validator.messages[0].message).toEqual('invalid url')
+      expect(validator.messages[0].field).toEqual('url')
+      expect(validator.messages[0].type).toEqual('invalid_url')
+    })
+
+    describe('with custom message', () => {
+      it('adds message', () => {
+        let validator = newValidator({url: 'https/github.com/gregors' })
+        expect(validator.messages.length).toBe(0)
+        validator = validateUrl(validator, 'url', { message: 'try again' })
+
+        expect(validator.messages.length).toBe(1)
+        expect(validator.messages[0].message).toEqual('try again')
+      })
+    })
+  })
+})
